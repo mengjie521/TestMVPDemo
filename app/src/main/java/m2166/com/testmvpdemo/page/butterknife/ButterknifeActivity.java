c7a31a9294209adf8849605640d85aa34e092f89
mengjie <weidadjie@163.com>
2017-08-21 10:33:20 +0800

    自定义的toast

--- app/src/main/java/m2166/com/testmvpdemo/page/butterknife/ButterknifeActivity.java
+++ app/src/main/java/m2166/com/testmvpdemo/page/butterknife/ButterknifeActivity.java
@@ -3,6 +3,9 @@ package m2166.com.testmvpdemo.page.butterknife;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -43,8 +46,22 @@ public class ButterknifeActivity extends Activity {
 
     }
 
-    @OnClick(R.id.iv_test)
-    public void onViewClicked() {
-        Toast.makeText(mActivity,"GGGGGGGGGG",Toast.LENGTH_SHORT).show();
+
+    @OnClick({R.id.bt_test, R.id.tv_test, R.id.iv_test})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bt_test:
+                View toastView = LayoutInflater.from(mActivity).inflate(R.layout.toast_custom, null);
+                Toast toast = new Toast(mActivity);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(toastView);
+                toast.show();
+                break;
+            case R.id.tv_test:
+                break;
+            case R.id.iv_test:
+                break;
+        }
     }
 }
